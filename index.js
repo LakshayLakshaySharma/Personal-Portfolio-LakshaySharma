6c225a005729ce5d966749d5147ba5afcbe6380d
Lakshay Sharma <Lakshay0sharma@gmail.com>
2023-10-23 04:23:33 +0530

    complete the full structure of our personal portfolio website

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var typed = new Typed('#element', {
-    strings: ['Web Designer', 'Graphic Designer', 'Content Writer.'],
+    strings: ['Web Designer', 'Graphic Designer', 'UI/UX Developer.'],
     typeSpeed: 50,
   });
 
+  $(document).ready(function() {
+    // Footer height fix
+    var footerHeight = $('footer').outerHeight();
+    $('body').css('padding-bottom', footerHeight);
+    $('footer').css('margin-top', -footerHeight);
+  });
